fa450341c2fc41ef9db2f131190afbfa60e488f2
petrep <petrep@freemail.hu>
2017-02-22 01:14:11 +0100

    monitor, disconnect and test buttons working. Todo: listen to CRUD actions, correct row ids

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,3 +1,10 @@
+// Working version!  However, this only worked on a local database (no remote yet)
+// Info: I am using Oracle 12c with ojdbc7.jar
+// The code was created based on: http://stackoverflow.com/questions/21697500/jdbc-oracle-database-change-notification-duplicate-events
+// Further reading: https://docs.oracle.com/database/121/JJDBC/dbchgnf.htm#JJDBC28820
+// SQL command to check if oracle db notifs are still active:
+// select TABLE_NAME from USER_CHANGE_NOTIFICATION_REGS
+
 package application;
 	
 import javafx.application.Application;
--- src/application/MainController.java
+++ src/application/MainController.java
@@ -1,7 +1,9 @@
 package application;
 
 import java.net.URL;
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.Properties;
 import java.util.ResourceBundle;
 
@@ -9,6 +11,10 @@ import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
 import oracle.jdbc.OracleConnection;
 import oracle.jdbc.OracleDriver;
+import oracle.jdbc.OracleStatement;
+import oracle.jdbc.dcn.DatabaseChangeEvent;
+import oracle.jdbc.dcn.DatabaseChangeListener;
+import oracle.jdbc.dcn.DatabaseChangeRegistration;
 
 public class MainController implements Initializable {
 	static final String USERNAME = "system";
@@ -16,6 +22,7 @@ public class MainController implements Initializable {
 	static String URL = "jdbc:oracle:thin:system/password@//localhost:1521/dbtracker1";
 	OracleConnection conn = null;
 	Properties prop = null;
+	DatabaseChangeRegistration dcr = null;
 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
@@ -35,19 +42,55 @@ public class MainController implements Initializable {
 		
 		conn = connect();
 		
+		prop.setProperty(OracleConnection.DCN_NOTIFY_ROWIDS, "true");
+		dcr = conn
+				.registerDatabaseChangeNotification(prop);
+		System.out.println("running1");
+		try {
+			dcr.addListener(new DatabaseChangeListener() {
+
+				public void onDatabaseChangeNotification(DatabaseChangeEvent dce) {
+					System.out.println("Changed row id : "
+							+ dce.getTableChangeDescription()[0]
+									.getRowChangeDescription()[0].getRowid()
+									.stringValue());
+				}
+			});
+			System.out.println("r2");
+			System.out.println(dcr.getRegId());
+			System.out.println(dcr.getState());
+			Statement stmt = conn.createStatement();
+			((OracleStatement) stmt).setDatabaseChangeRegistration(dcr);
+			ResultSet rs = stmt.executeQuery("select * from table1 where id=1");
+			while (rs.next()) {
+				System.out.println(rs.getString("col3"));
+			}
+			rs.close();
+			stmt.close();
+		} catch (SQLException ex) {
+			if (conn != null) {
+				conn.unregisterDatabaseChangeNotification(dcr);
+				conn.close();
+				System.out.println("closed");
+			}
+			throw ex;
+		}
+		
+		
 		System.out.println("The connectDB method ends");
 	}
 	
 	public void disconnectDB(ActionEvent event) throws SQLException{
 		System.out.println("The disconnectDB method starts");
-
+		System.out.println(dcr.getState());
 //		conn.abort();
 //		conn.close(prop);
 		if(conn!=null) {
+			conn.unregisterDatabaseChangeNotification(dcr);
 			conn.close();
-		
-//		conn = null;
+//			conn = null;
 			System.out.println("connection is closed: " + conn.isClosed());
+			System.out.println("db notifications stopped");
 		}
 		
 		System.out.println("The disconnectDB method ends");
